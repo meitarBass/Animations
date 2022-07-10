908b83716430dc44e3e07d42304d174f7ab5a11e
meitarbasson <meitarbass@gmail.com>
2022-07-10 09:10:23 +0300

    Nice first animation.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+//            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
