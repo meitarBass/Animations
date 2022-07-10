a11bf12bc35d6279b9dfab6713fefe20a852760a
meitarbasson <meitarbass@gmail.com>
2022-07-10 09:19:18 +0300

    Day 32 finished.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,31 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var animationAmount = 1.0
 
+    @State private var animationAmount = 0.0
+
+    
     var body: some View {
         Button("Tap Me") {
-//            animationAmount += 1
+            // do nothing
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+        
+
     }
 }
 
